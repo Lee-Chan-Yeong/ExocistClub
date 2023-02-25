a72edfa45f9ec64e0272f2dd51029986b7a844a5
Lee-Chan_Yeong <tncjq39@gmail.com>
2023-02-25 22:12:33 +0900

    [기능 추가] 애니메이션 수정

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Hammer : MonoBehaviour
 {
+    //우사기 선택 여부
     public bool ChooseUsagie = false;
     Player player;
+    //기본공격의 공격속도
     float delayTime= 0;
     int atkDamage =0;
     float waitTime;
+    //기본공격의 애니메이션 속도
+    float ubasicAttackSpeed;
     
 
     Animator anim;
+    Collider2D AtkCol;
      
 
     // Start is called before the first frame update
@@ -19,6 +24,9 @@ public class Hammer : MonoBehaviour
     {
         player = GetComponentInParent<Player>();
         anim = GetComponent<Animator>();
+        AtkCol = GetComponent<Collider2D>();
+        AtkCol.enabled = false;
+        
         delayTime = player.playerUnitStat.defaultPlayerAtkDelay;
         atkDamage = player.playerUnitStat.defaultPlayerAtk;
     }
@@ -26,16 +34,17 @@ public class Hammer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        basicAttack();
+        ubasicAttack();
     }
 
-    void basicAttack()
+    void ubasicAttack()
     {   
         waitTime += Time.deltaTime;
         if (waitTime >= delayTime)
         {
             Debug.Log("슉");
             anim.SetTrigger("Attack");
+            StartCoroutine(uBasicAtkEnabled());
             waitTime = 0;
         }
         
@@ -46,5 +55,11 @@ public class Hammer : MonoBehaviour
         Debug.Log("뿅");
     }
 
+    IEnumerator uBasicAtkEnabled()
+    {
+        AtkCol.enabled = true;
+        yield return new WaitForSeconds(0.3f);
+        AtkCol.enabled = false;
+    }
 
 }  
